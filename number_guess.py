8fb5fd2a3054c6e3e47f16f7afc8af87b3f1a069
the-real-anonymous-codes <techbylalit01@gmail.com>
2026-07-31 19:22:42 +0530

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -2,13 +2,13 @@ import random
 secret_num = random.choice(range(101))
 print(secret_num)
 
-for i in range (1) :
+for i in range (5) :
     guess_num = int(input("Enter your guess number: "))
     if guess_num == (secret_num):
         print("YOU WON YOU DOMINATED THE WHOLE GAME")
         break
     
-    elif guess_num > (secret_num):
+    elif  guess_num > secret_num  :
         print("You went higher, come to a lower value")
     
     else:
